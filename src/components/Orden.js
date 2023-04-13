e81ed6a80b917e76c0adc2cef529d36d6acb9890
Nosferatu721 <elkintorres721@gmail.com>
2023-04-12 22:28:33 -0500

    Finish LPM

--- src/components/Orden.js
+++ src/components/Orden.js
@@ -1,8 +1,19 @@
 import { formatearDinero } from '@/helpers';
+import axios from 'axios';
 import Image from 'next/image';
+import { toast } from 'react-toastify';
 
 const Orden = ({ orden }) => {
   const { id, nombre, total, pedido } = orden;
+
+  const completarOrden = async () => {
+    try {
+      const data = await axios.post(`/api/ordenes/${id}`);
+      toast.success('Orden Lista');
+    } catch (error) {
+      toast.error('Hubo un error');
+    }
+  };
 
   return (
     <div className="border p-10 space-y-5">
@@ -15,20 +26,24 @@ const Orden = ({ orden }) => {
             <div className="w-32">
               <Image width={400} height={500} src={`/assets/img/${platillo.imagen}.jpg`} alt="Imagen Platillo" />
             </div>
-            <div className='p-5 space-y-2'>
-              <h4 className='text-xl font-bold text-amber-500'>{platillo.nombre}</h4>
-              <p className='text-lg font-bold'>Cantidad: {platillo.cantidad}</p>
+            <div className="p-5 space-y-2">
+              <h4 className="text-xl font-bold text-amber-500">{platillo.nombre}</h4>
+              <p className="text-lg font-bold">Cantidad: {platillo.cantidad}</p>
             </div>
           </div>
         ))}
       </div>
 
-      <div className='md:flex md:items-center md:justify-between my-10'>
-        <p className='mt-5 font-black text-4xl text-amber-500'>
-          Total a pagar: {formatearDinero(total)}
-        </p>
+      <div className="md:flex md:items-center md:justify-between my-10">
+        <p className="mt-5 font-black text-4xl text-amber-500">Total a pagar: {formatearDinero(total)}</p>
 
-        <button className='bg-indigo-600 hover:bg-indigo-800 text-white mt-5 md:mt-0 py-3'>Completar Orden</button>
+        <button
+          className="bg-indigo-600 hover:bg-indigo-800 text-white mt-5 md:mt-0 py-3 px-10 uppercase font-bold rounded-lg"
+          type="button"
+          onClick={completarOrden}
+        >
+          Completar Orden
+        </button>
       </div>
     </div>
   );
